0170cf1990450d74900e6b11e0578dc2c02138f8
Andreas Blaesius <skate4life@gmx.de>
2016-03-22 16:50:17 +0100

    Rename espresso to espressowifi to avoid conflicts with HTC's espresso
    (myTouch 3G Slide T-Mobile - "espresso" - exist already )
    
    Change-Id: I38ca47a705139241e7091d6f236ecda608c651cd

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # Include espresso-common makefile
-$(call inherit-product, device/samsung/espresso/espresso-common.mk)
+$(call inherit-product, device/samsung/espressowifi/espresso-common.mk)
 
 # Include 3g overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/aosp
