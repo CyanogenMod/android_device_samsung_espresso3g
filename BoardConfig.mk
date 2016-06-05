ca9e00551d6c2eb6e51f7039860fee39573a5a73
Andreas Blaesius <skate4life@gmx.de>
2016-06-05 20:43:59 +0200

    Add espresso3g to TARGET_OTA_ASSERT_DEVICE
    Change-Id: I72e248f2916eef9e29658a709aa62b8fea8904f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,4 +25,4 @@ BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6260
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := espresso-common,p3100,GT-P3100,espressorf,espressorfxx,p5100,GT-P5100,espresso10rf,espresso10rfxx
+TARGET_OTA_ASSERT_DEVICE := espresso3g,espresso-common,p3100,GT-P3100,espressorf,espressorfxx,p5100,GT-P5100,espresso10rf,espresso10rfxx
