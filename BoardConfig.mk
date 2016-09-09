613e11cf7eaf8fb8cc2ccc795ce24127832d7547
Ziyan <jaraidaniel@gmail.com>
2016-09-09 13:48:59 +0200

    Remove custom libril
    PS2: remove custom rom specific dependencies of hardware/samsung repo
    
    Change-Id: I561608662bc207a208da81d461a2532ca0a2f5af

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,12 +17,5 @@
 # Include common espresso BoardConfig
 -include device/samsung/espressowifi/BoardConfigCommon.mk
 
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso3g/include
-
-# RIL
-BOARD_VENDOR := samsung
-BOARD_PROVIDES_LIBRIL := true
-BOARD_MODEM_TYPE := xmm6260
-
 # assert
 TARGET_OTA_ASSERT_DEVICE := espresso3g,espresso-common,p3100,GT-P3100,espressorf,espressorfxx,p5100,GT-P5100,espresso10rf,espresso10rfxx
